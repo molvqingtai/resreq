5d87a7220ea1e7aca0e96c85ca68ea961f8b22b4
molvqingtai <molvqingtai@gmail.com>
2022-03-17 14:37:45 +0800

    perf: move stream processing to middleware

--- src/Res.ts
+++ src/Res.ts
@@ -1,46 +1,5 @@
 import { ON_GLOBAL_REQUEST_PROGRESS, ON_GLOBAL_RESPONSE_PROGRESS } from './consts'
 
-const readStream = (response: Response, onResponseProgress: ProgressCallback) =>
-  new ReadableStream({
-    async start(controller) {
-      /**
-       * When http compression is used (common for big downloads)
-       * The content-length is the size after the http compression
-       * While the byteLength is the size after the file has been extracted.
-       */
-      const total = +response.headers.get('content-length')! || 0
-      const reader = response.clone().body!.getReader()
-      let carry = 0
-
-      onResponseProgress(
-        {
-          ratio: 0, // Current Transfer Ratio
-          carry: 0, // Current Transfer Byte Size
-          total: 0 // Total size of transmitted bytes
-        },
-        new Uint8Array() // The chunk argument is an instance of Uint8Array.
-      )
-      const read = async () => {
-        const { done, value } = await reader.read()
-        if (done) return controller.close()
-
-        carry += value!.byteLength
-
-        onResponseProgress(
-          {
-            ratio: (carry / total) * 100,
-            carry,
-            total
-          },
-          value!
-        )
-        controller.enqueue(value)
-        await read()
-      }
-      await read()
-    }
-  })
-
 export interface ResInit extends ResponseInit, ExtendInit {
   [ON_GLOBAL_REQUEST_PROGRESS]?: ProgressCallback
   [ON_GLOBAL_RESPONSE_PROGRESS]?: ProgressCallback
@@ -57,7 +16,6 @@ export default class Res extends Response {
   readonly [ON_GLOBAL_REQUEST_PROGRESS]?: ProgressCallback;
   readonly [ON_GLOBAL_RESPONSE_PROGRESS]?: ProgressCallback
   constructor(response: Response, init?: ResInit) {
-    const _response = response.clone()
     super(response.body, {
       status: init?.status ?? response.status,
       statusText: init?.statusText ?? response.statusText,
@@ -71,9 +29,5 @@ export default class Res extends Response {
     this.onResponseProgress = init?.onResponseProgress
     this[ON_GLOBAL_REQUEST_PROGRESS] = init?.[ON_GLOBAL_REQUEST_PROGRESS]
     this[ON_GLOBAL_RESPONSE_PROGRESS] = init?.[ON_GLOBAL_RESPONSE_PROGRESS]
-    readStream(_response, (...args) => {
-      this.onResponseProgress?.(...args)
-      this[ON_GLOBAL_RESPONSE_PROGRESS]?.(...args)
-    })
   }
 }
--- src/middleware/responseHandler.ts
+++ src/middleware/responseHandler.ts
@@ -1,11 +1,60 @@
 import { Middleware } from '../index'
 import Res from '../Res'
+import { ON_GLOBAL_RESPONSE_PROGRESS } from '../consts'
+
+const readStream = (response: Response, onResponseProgress: ProgressCallback) =>
+  new ReadableStream({
+    async start(controller) {
+      /**
+       * When http compression is used (common for big downloads)
+       * The content-length is the size after the http compression
+       * While the byteLength is the size after the file has been extracted.
+       */
+      const total = +response.headers.get('content-length')! || 0
+      const reader = response.clone().body!.getReader()
+      let carry = 0
+
+      onResponseProgress(
+        {
+          ratio: 0, // Current Transfer Ratio
+          carry: 0, // Current Transfer Byte Size
+          total: 0 // Total size of transmitted bytes
+        },
+        new Uint8Array() // The chunk argument is an instance of Uint8Array.
+      )
+      const read = async () => {
+        const { done, value } = await reader.read()
+        if (done) return controller.close()
+
+        carry += value!.byteLength
+
+        onResponseProgress(
+          {
+            ratio: (carry / total) * 100,
+            carry,
+            total
+          },
+          value!
+        )
+        controller.enqueue(value)
+        await read()
+      }
+      await read()
+    }
+  })
 
 const responseHandler: Middleware = (next) => async (req) => {
   const response = await next(req)
+
   if (req.throwHttpErrors && !response.ok) {
     throw new Error(`${response.status} ${response.statusText}`)
   }
+
+  readStream(response.clone(), (...args) => {
+    response.onResponseProgress?.(...args)
+    req[ON_GLOBAL_RESPONSE_PROGRESS]?.(...args)
+  })
+
   return new Res(response, req)
 }
 
